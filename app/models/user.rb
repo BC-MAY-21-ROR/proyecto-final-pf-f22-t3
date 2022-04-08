1966dde45b9a7406e324a82b5a39c25ffeadbe60
Alex S <alexs1010@gmail.com>
2022-04-08 17:23:42 -0500

    Add model wallet and associated with user set initial balance after create user close issue #4
    Co-authored-by: AlejandroCuevasZepeda <AlejandroCuevasZepeda@users.noreply.github.com>
    Co-authored-by: alanRazoMen <alanRazoMen@users.noreply.github.com>
    Co-authored-by: MrCano369x <MrCano369x@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one :wallet
+  after_create :create_wallet
+
+  def create_wallet
+    build_wallet(coins: 50.00).save
+  end
 end
