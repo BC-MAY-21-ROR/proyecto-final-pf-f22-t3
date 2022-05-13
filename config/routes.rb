fce26b2e4bd920ba6ed2bcff6a6fbeb09032ce7d
Alex S <alexs1010@gmail.com>
2022-05-13 13:39:07 -0500

    Add ability to edit profile registration data, can change email and password

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  resources :users, only: [:show, :edit, :update]
+
   get "/cheat", to: "minigames#cheat"
   get "/spin", to: "minigames#spin"
 
